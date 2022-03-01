81cc0fade4aca528fd28fbcb0a3421011b505c9a
KarmaTasticDEV <67900447+Dr-Insanity@users.noreply.github.com>
2022-03-01 15:23:21 +0100

    s

--- main.py
+++ main.py
@@ -10,16 +10,25 @@ class NetFucker:
 
     class Stealth:
         def ARPScan(target_ip: str, blocked_host: str):
-            print(f"{NeTfUcKeR()} Lights out. Bravo-6, going dark. Commencing Stealthy ARP Scan...")
+            def countdown(t: int):
+                while t:
+                    mins, secs = divmod(t, 60)
+                    timer = NeTfUcKeR()+' Lights out. Bravo-6, going dark. Commencing Stealthy ARP Scan... [{:02d}:{:02d}]'.format(mins, secs)
+                    print(timer, end="\r")
+                    time.sleep(1)
+                    t -= 1
+
             # MAINTAIN STEALTH!!! Wait before we cause noise for our sys admin here... 
             # Let's appear "legit" by sending ARP requests 
             # less frequently, you know, like every other device?
             # WAIT at least a minute
-            def break_wait_pattern() -> int:
+            def break_wait_pattern():
                 """## To maintain absolute stealth, we don't want to let our sysadmin notice a pattern in our scans.\n## Therefore, we don't neccessarrily wait 60 seconds.. but > 60 seconds (sometimes more)"""
-                return random.randint(60, 150)
+                return random.uniform(60.0, 150.0)
 
-            time.sleep(break_wait_pattern())
+            t = break_wait_pattern()
+            countdown(t)
+            time.sleep(t)
 
             sniff(target_ip, blocked_host)
     class Appearance:
@@ -31,12 +40,15 @@ class NetFucker:
  | . ` |/ _ \ __|  __| | | |/ __| |/ / _ \ '__|
  | |\  |  __/ |_| |  | |_| | (__|   <  __/ |   
  |_| \_|\___|\__|_|   \__,_|\___|_|\_\___|_|   
-            """ + Fore.GREEN + """
+            """ + Fore.LIGHTGREEN_EX + """
 We DGAF about your internet connection. We own it.""")
             return True # It went through fine. Quit this function
 
 def NeTfUcKeR():
-    return Fore.WHITE + "[" + Fore.RED + "N" + Fore.YELLOW + "e" + Fore.GREEN + "t" + Fore.MAGENTA + "F" + Fore.CYAN + "u" + Fore.BLUE + "c" + Fore.RED + "K" + Fore.YELLOW + "e" + Fore.GREEN + "r" + Fore.WHITE + "]"
+    return Fore.WHITE + "[" + Fore.RED + "N" + Fore.YELLOW + "e" + Fore.LIGHTGREEN_EX + "t" + Fore.MAGENTA + "F" + Fore.CYAN + "u" + Fore.BLUE + "c" + Fore.RED + "K" + Fore.YELLOW + "e" + Fore.LIGHTGREEN_EX + "r" + Fore.WHITE + "]"
+
+def print_DiscoveringStage():
+    print(Fore.GREEN + "==================" + Fore.GREEN + "[" + Fore.BLUE + "D" + Fore.RED + "I" + Fore.YELLOW + "S" + Fore.LIGHTGREEN_EX + "C" + Fore.MAGENTA + "O" + Fore.CYAN + "V" + Fore.BLUE + "E" + Fore.RED + "R" + Fore.YELLOW + "Y" + Fore.CYAN + "S" + Fore.BLUE + "T" + Fore.RED + "A" + Fore.YELLOW + "G" + Fore.LIGHTGREEN_EX + "E" + Fore.GREEN + "]" + Fore.GREEN + "==================")
 
 def main():
     NetFucker.Appearance.printBanner()
@@ -44,6 +56,7 @@ def main():
     print(f"{NeTfUcKeR()} > Target set.")
     blocked_host = input(f"{NeTfUcKeR()} OK, then what IPv4 address shouldn't they be visiting? (example: The IPv4 address of youtube.com)> ")
     print(f"{NeTfUcKeR()} > Filter set.")
+    print_DiscoveringStage()
     print(f"{NeTfUcKeR()} Checking if given target IPv4 is currently giving away their existence. Please wait...")
     #gateway_ip = input(f"{NeTfUcKeR()} Gateway IP address? > ")
     p = sniff(target_ip=target_ip, blocked_host=blocked_host)
